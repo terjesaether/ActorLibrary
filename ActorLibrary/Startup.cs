f0592fd87a7cd972043745c6a6445f43a5758bbd
terje <terje@MBP-WIN>
2016-09-16 18:58:31 +0200

    Add project files.

---
+++ ActorLibrary/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ActorLibrary.Startup))]
+namespace ActorLibrary
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
